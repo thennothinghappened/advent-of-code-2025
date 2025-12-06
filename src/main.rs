9fdcb7b3fc91132532baa9cba18de3a73b560ee5
orca <35243139+thennothinghappened@users.noreply.github.com>
2025-12-06 15:26:04 +1100

    feat: allow running specific day

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::read_to_string, time::Instant};
+use std::{env::args, fs::read_to_string, time::Instant};
 
 use owo_colors::OwoColorize;
 
@@ -6,34 +6,49 @@ mod day01;
 mod day02;
 
 type DayFunc = fn(&str) -> anyhow::Result<()>;
-const DAYS: &[(&str, DayFunc)] = &[("Secret Entrance", day01::run), ("Gift Shop", day02::run)];
+type Day = (&'static str, DayFunc);
+
+const DAYS: &[Day] = &[("Secret Entrance", day01::run), ("Gift Shop", day02::run)];
 
 fn main() {
-    let mut day_number = 1;
+    if let Some(day_number) = args().nth(1).and_then(|it| it.parse::<usize>().ok()) {
+        let Some(day) = DAYS.get(day_number - 1) else {
+            println!("Day {day_number} not implemented!");
+            return;
+        };
 
-    for (title, day) in DAYS {
-        let formatted_day_number = format!("{day_number:02}");
-        println!(
-            "{}",
-            format!("-- Day {formatted_day_number}: {title} --")
-                .blue()
-                .bold()
-        );
+        run_day(day_number, day);
+    } else {
+        let mut day_number = 1;
 
-        if let Ok(input) = read_to_string(format!("src/day{formatted_day_number}.txt")) {
-            let start_time = Instant::now();
+        for day in DAYS {
+            run_day(day_number, day);
+            day_number += 1;
+        }
+    }
+}
 
-            match day(&input) {
-                Ok(_) => println!("\n{}", "OK!".green()),
-                Err(error) => println!("\n{} {error:?}", "Error:".red()),
-            }
+fn run_day(day_number: usize, day: &Day) {
+    let formatted_day_number = format!("{day_number:02}");
+    println!(
+        "{}",
+        format!("-- Day {formatted_day_number}: {} --", day.0)
+            .blue()
+            .bold()
+    );
 
-            println!("Took {:.2?}", start_time.elapsed());
-        } else {
-            println!("(Skipped: no input)");
+    if let Ok(input) = read_to_string(format!("src/day{formatted_day_number}.txt")) {
+        let start_time = Instant::now();
+
+        match day.1(&input) {
+            Ok(_) => println!("\n{}", "OK!".green()),
+            Err(error) => println!("\n{} {error:?}", "Error:".red()),
         }
 
-        println!();
-        day_number += 1;
+        println!("Took {:.2?}", start_time.elapsed());
+    } else {
+        println!("(Skipped: no input)");
     }
+
+    println!();
 }
