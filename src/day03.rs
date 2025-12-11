9f7537137ce5af75e2fcb4cbfd1b49b25e2ebcbf
orca <35243139+thennothinghappened@users.noreply.github.com>
2025-12-11 13:12:41 +1100

    refactor(day 3): slightly more readable

--- src/day03.rs
+++ src/day03.rs
@@ -2,7 +2,7 @@ use anyhow::{Context, Result, anyhow};
 use itertools::Itertools;
 
 pub fn run(input: &str) -> anyhow::Result<()> {
-    let banks = input.lines();
+    let banks = input.lines().map(|line| line.as_bytes());
 
     let mut p1_jolts: u64 = 0;
     let mut p2_jolts: u64 = 0;
@@ -21,26 +21,23 @@ pub fn run(input: &str) -> anyhow::Result<()> {
     Ok(())
 }
 
-fn bank_calculate_joltage(bank: &str, digits: u16) -> u64 {
+fn bank_calculate_joltage(bank: &[u8], digits: u32) -> u64 {
     let mut best_index = 0;
     let mut best_digit = 0;
 
-    for (index, digit) in bank
-        .bytes()
-        .dropping_back(digits as usize)
-        .map(|b| b - b'0')
-        .enumerate()
-    {
+    for (index, &digit) in bank.iter().dropping_back(digits as usize).enumerate() {
         if digit > best_digit {
             best_index = index;
             best_digit = digit;
         }
     }
 
+    best_digit -= b'0';
+
     if digits == 0 {
         return best_digit as u64;
     }
 
-    (best_digit as u64) * 10_u64.pow(digits as u32)
+    (best_digit as u64) * 10_u64.pow(digits)
         + bank_calculate_joltage(&bank[best_index + 1..], digits - 1)
 }
