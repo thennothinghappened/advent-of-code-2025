8d480eed974afe8c5fa00dc5020ce40be3f94fd4
orca <35243139+thennothinghappened@users.noreply.github.com>
2025-12-11 13:02:15 +1100

    feat: day 3 part 2

--- src/day03.rs
+++ src/day03.rs
@@ -4,31 +4,43 @@ use itertools::Itertools;
 pub fn run(input: &str) -> anyhow::Result<()> {
     let banks = input.lines();
 
-    let p1_jolts = banks
-        .map(|bank| {
-            let mut tens_index = 0;
-            let mut tens_digit = 0;
+    let mut p1_jolts: u64 = 0;
+    let mut p2_jolts: u64 = 0;
 
-            for (index, digit) in bank.bytes().dropping_back(1).map(|b| b - b'0').enumerate() {
-                if digit > tens_digit {
-                    tens_index = index;
-                    tens_digit = digit;
-                }
-            }
+    for bank in banks {
+        // Part 1: 2-digit number.
+        p1_jolts += bank_calculate_joltage(bank, 1);
 
-            let mut ones_digit = 0;
-
-            for digit in bank.bytes().dropping(tens_index + 1).map(|b| b - b'0') {
-                if digit > ones_digit {
-                    ones_digit = digit;
-                }
-            }
-
-            ((tens_digit * 10) + ones_digit) as u32
-        })
-        .sum::<u32>();
+        // Part 2: 12-digit number.
+        p2_jolts += bank_calculate_joltage(bank, 11);
+    }
 
     println!("Part 1: {p1_jolts}");
+    println!("Part 2: {p2_jolts}");
 
     Ok(())
+}
+
+fn bank_calculate_joltage(bank: &str, digits: u16) -> u64 {
+    let mut best_index = 0;
+    let mut best_digit = 0;
+
+    for (index, digit) in bank
+        .bytes()
+        .dropping_back(digits as usize)
+        .map(|b| b - b'0')
+        .enumerate()
+    {
+        if digit > best_digit {
+            best_index = index;
+            best_digit = digit;
+        }
+    }
+
+    if digits == 0 {
+        return best_digit as u64;
+    }
+
+    (best_digit as u64) * 10_u64.pow(digits as u32)
+        + bank_calculate_joltage(&bank[best_index + 1..], digits - 1)
 }
